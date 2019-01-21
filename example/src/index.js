73183f62e118a0444a4232de8a4bd52827d6709d
zenghongtu <zenghongtu@gmail.com>
2019-01-21 20:34:10 +0800

    add example history basename

--- example/src/index.js
+++ example/src/index.js
@@ -7,8 +7,12 @@ import routes from './router.config'
 import {Provider} from 'react-redux'
 import store from './store'
 
+const historyOpts = {
+  basename: '/react-config-router'
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    {createRouter(routes)}
+    {createRouter(routes, {historyOpts})}
   </Provider>
   , document.getElementById('root'))
